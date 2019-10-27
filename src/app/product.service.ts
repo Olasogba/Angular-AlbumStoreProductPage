8e5ff486a0b93dc8db35b64bc2d35f2093ba65cb
Michael akinademola <Michael.akinademola@quomodosystems.com>
2019-10-27 11:31:23 +0100

    commit

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -9,11 +9,9 @@ private _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
 
   public getAlbum(id: number) {
-    return this._http.get(this._albumUrl).pipe(
-      map( response => {
+    return this._http.get(this._albumUrl).map( response => {
         response.json();
       }
-      )
-    );
+      );
   }
 }
